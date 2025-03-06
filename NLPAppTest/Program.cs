609218e4ac7db56f5cca444046a67108f32b7af5
FrizyClop <wifikin@mail.ru>
2025-03-06 16:42:10 +0300

    Добавьте файлы проекта.

---
+++ NLPAppTest/Program.cs
@@ -0,0 +1,19 @@
+﻿using SimpleNLP;
+
+// Создаём экземпляр класса NLPProcessor
+var nlpProcessor = new NLPProcessor();
+
+// Чтение из файла
+string file_name = "Examples\\SSD№01.txt";
+StreamReader stream_reader = new StreamReader(file_name);
+string text = stream_reader.ReadToEnd();
+stream_reader.Close();
+
+// Анализируем текст
+var wordFrequency = nlpProcessor.AnalyzeText(text);
+
+// Выводим результаты
+foreach (var word_frequency in wordFrequency)
+{
+    Console.WriteLine($"{word_frequency.Key}: {word_frequency.Value}");
+}
